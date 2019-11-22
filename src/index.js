0c11d61100e9055e3dd70e0b7b62a80f86566fd2
Miko Kari <miko.kari@outlook.com>
2019-11-22 13:55:19 +0200

    added StarPicker as named export for backwards compatibility

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { default } from './StarPicker';
+export { default, default as StarPicker } from './StarPicker';
 export { default as defaultStarRenderer } from './defaultStarRenderer';
 export {
   default as DefaultStarRendererStarComponent,
