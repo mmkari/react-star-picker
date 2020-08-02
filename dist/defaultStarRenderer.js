6af738786d803453324b3617c99e362b553b666c
Miko Kari <miko.kari@outlook.com>
2020-08-02 13:36:47 +0300

    rebuilt dist

--- dist/defaultStarRenderer.js
+++ dist/defaultStarRenderer.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = void 0;
+exports["default"] = exports.defaultColors = exports.mixColorRemove = exports.mixColorAdd = void 0;
 
 var React = _interopRequireWildcard(require("react"));
 
@@ -17,6 +17,31 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; if (obj != null) { var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
+var defaultColorActive = 'gold';
+var defaultColorInactive = '#e4e4e4';
+
+var mixColorAdd = function mixColorAdd(active, inactive) {
+  return (0, _polished.mix)(0.2, active, inactive);
+};
+
+exports.mixColorAdd = mixColorAdd;
+
+var mixColorRemove = function mixColorRemove(active, inactive) {
+  return (0, _polished.mix)(0.6, inactive, active);
+};
+
+exports.mixColorRemove = mixColorRemove;
+var defaultColors = {
+  colorActive: defaultColorActive,
+  colorInactive: defaultColorInactive,
+  // a light gray
+  colorAdd: mixColorAdd(defaultColorActive, defaultColorInactive),
+  // mix colors for cue on stars to be added
+  colorRemove: mixColorRemove(defaultColorActive, defaultColorInactive) // mix colors for cue on stars to be removed
+
+};
+exports.defaultColors = defaultColors;
+
 var defaultStarRenderer = function defaultStarRenderer(_ref) {
   var index = _ref.index,
       selectedIndex = _ref.selectedIndex,
@@ -24,13 +49,13 @@ var defaultStarRenderer = function defaultStarRenderer(_ref) {
       starRendererProps = _ref.starRendererProps;
   // default renderer props:
   var _starRendererProps$co = starRendererProps.colorActive,
-      colorActive = _starRendererProps$co === void 0 ? 'gold' : _starRendererProps$co,
+      colorActive = _starRendererProps$co === void 0 ? defaultColors.colorActive : _starRendererProps$co,
       _starRendererProps$co2 = starRendererProps.colorInactive,
-      colorInactive = _starRendererProps$co2 === void 0 ? '#e4e4e4' : _starRendererProps$co2,
+      colorInactive = _starRendererProps$co2 === void 0 ? defaultColors.colorInactive : _starRendererProps$co2,
       _starRendererProps$co3 = starRendererProps.colorAdd,
-      colorAdd = _starRendererProps$co3 === void 0 ? (0, _polished.mix)(0.2, colorActive, colorInactive) : _starRendererProps$co3,
+      colorAdd = _starRendererProps$co3 === void 0 ? mixColorAdd(colorActive, colorInactive) || defaultColors.colorAdd : _starRendererProps$co3,
       _starRendererProps$co4 = starRendererProps.colorRemove,
-      colorRemove = _starRendererProps$co4 === void 0 ? (0, _polished.mix)(0.6, colorInactive, colorActive) : _starRendererProps$co4,
+      colorRemove = _starRendererProps$co4 === void 0 ? mixColorRemove(colorActive, colorInactive) || defaultColors.colorRemove : _starRendererProps$co4,
       _starRendererProps$ch = starRendererProps.charCodeSelected,
       charCodeSelected = _starRendererProps$ch === void 0 ? 9733 : _starRendererProps$ch,
       _starRendererProps$ch2 = starRendererProps.charCodeUnselected,
