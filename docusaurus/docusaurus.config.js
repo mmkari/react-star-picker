c85652fe71d7d95bb6ac46f04aca2abd7cc4d215
Miko Kari <miko.kari@outlook.com>
2023-02-10 23:38:25 +0200

    github link

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -72,7 +72,7 @@ const config = {
           },
           // { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/mmkari/react-star-picker',
             label: 'GitHub',
             position: 'right',
           },
