afe641c18c99c1b63071a8ae81a08f63fe56fd29
Miko Kari <miko.kari@outlook.com>
2023-02-12 22:56:11 +0200

    install & config docgen

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -54,6 +54,29 @@ const config = {
     ],
   ],
 
+  plugins: [
+    [
+      'docusaurus-plugin-react-docgen-typescript',
+      {
+        /** @type {import('docusaurus-plugin-react-docgen-typescript').Options} */
+        // pass in a single string or an array of strings
+        src: ['../src/*.tsx'], // , '!path/to/**/*test.*'
+        parserOptions: {
+          // pass parserOptions to react-docgen-typescript
+          // here is a good starting point which filters out all
+          // types from react
+          propFilter: (prop, component) => {
+            if (prop.parent) {
+              return !prop.parent.fileName.includes('@types/react');
+            }
+
+            return true;
+          },
+        },
+      },
+    ],
+  ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
