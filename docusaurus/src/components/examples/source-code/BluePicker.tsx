259e60bb825395a95b7e68baa0f3658cc335557c
Miko Kari <miko.kari@outlook.com>
2023-02-12 19:03:47 +0200

    blue source

--- docusaurus/src/components/examples/source-code/BluePicker.tsx
+++ docusaurus/src/components/examples/source-code/BluePicker.tsx
@@ -4,13 +4,13 @@ import MyComponentSource from '!!raw-loader!@site/src/components/HomepageFeature
 import MyComponentStyleSource from '!!raw-loader!@site/src/css/BluePicker.css';
 
 const BluePicker = () => {
-  return 'ASD';
-  // return (
-  //   <SourceCodeSection
-  //     componentSource={MyComponentSource}
-  //     componentStyleSource={MyComponentStyleSource}
-  //   />
-  // );
+  // return 'ASD';
+  return (
+    <SourceCodeSection
+      componentSource={MyComponentSource}
+      componentStyleSource={MyComponentStyleSource}
+    />
+  );
 };
 
 export default BluePicker;
