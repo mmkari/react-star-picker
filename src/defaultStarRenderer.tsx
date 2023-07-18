c2993e66bb3b98f331e8e99fa5aa60ab076d8b45
Miko Kari <miko.kari@outlook.com>
2023-07-18 20:33:32 +0300

    remove classes from default star

--- src/defaultStarRenderer.tsx
+++ src/defaultStarRenderer.tsx
@@ -3,26 +3,9 @@ import classnames from 'classnames';
 
 import './defaultStarRenderer.css';
 
-import { StarRendererProps } from './types';
-
-const defaultStarRenderer = ({
-  index,
-  selectedIndex,
-  hoverIndex,
-}: StarRendererProps): React.ReactNode => {
-  const selected = index <= selectedIndex;
-  const hoverActive = hoverIndex != null;
-  const inHoverRange = hoverActive && index <= hoverIndex;
-
+const defaultStarRenderer = (): React.ReactNode => {
   return (
-    <div
-      className={classnames('DefaultStarComponent', {
-        selected,
-        addSelection: hoverActive && !selected && inHoverRange,
-        removeSelection: hoverActive && selected && !inHoverRange,
-        isHovered: hoverIndex === index,
-      })}
-    >
+    <div className={classnames('DefaultStarComponent')}>
       {String.fromCharCode(9733)}
     </div>
   );
