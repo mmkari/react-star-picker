4fee3d67d991c9caa37a05e862fb03453b6f70b1
Miko Kari <miko.kari@outlook.com>
2024-03-10 20:33:39 +0200

    fixed typing

--- src/types.ts
+++ src/types.ts
@@ -37,7 +37,7 @@ export type StarPickerButtonProps = SharedProps &
 // eslint-disable-next-line no-undef
 export type StarPickerProps = Partial<SharedProps> & {
   /** function called with the selected value (and the input name) after a new pick */
-  onChange: (index: number | null, name?: string) => void;
+  onChange: (value: number | null, name?: string) => void;
   /** current value of the input */
   value: number | null;
   /** input's name, supplied to the onChange function if provided */
