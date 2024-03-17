c29200e70fe45e8fad86deb93fd3b2824dbc46ed
Miko Kari <miko.kari@outlook.com>
2024-03-18 00:03:33 +0200

    config

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -92,9 +92,9 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Home',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'React Star Picker Logo',
           src: 'img/star_favicon.svg',
         },
         items: [
