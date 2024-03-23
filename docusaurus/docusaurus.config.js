1316a07b00ebe864be1ad1d1f9fee358bb5ec195
Miko Kari <miko.kari@outlook.com>
2024-03-23 20:11:41 +0200

    update org name

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'https://github.com/mmkari', // Usually your GitHub org/user name.
+  organizationName: 'mmkari', // Usually your GitHub org/user name.
   projectName: 'react-star-picker', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
