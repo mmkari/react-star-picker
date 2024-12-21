43e65a4bf3e9395e24301532c1d650b823a4d608
Miko Kari <miko.kari@outlook.com>
2024-12-21 16:20:43 +0200

    add rotation

--- docusaurus/src/components/HomepageFeatures/FacePicker.tsx
+++ docusaurus/src/components/HomepageFeatures/FacePicker.tsx
@@ -11,6 +11,12 @@ function getRandomColor() {
   return `rgba(${r}, ${g}, ${b}, ${a})`;
 }
 
+// Function to generate a random rotation between -10deg and +10deg
+function getRandomRotation() {
+  const rotation = Math.floor(Math.random() * 21) - 10; // Random value between -10 and +10
+  return `${rotation}deg`;
+}
+
 const faces = ['🤬', '😖', '😠', '🙄', '😐', '🤨', '😁', '🥳', '🤩', '🦄'];
 
 const faceRenderer = ({ index, selectedIndex }) => {
@@ -24,13 +30,23 @@ const faceRenderer = ({ index, selectedIndex }) => {
 export const BluePicker = () => {
   const [rating, setRating] = React.useState<number | null>(null);
 
-  // Recalculate the ripple color whenever the rating changes
+  // Recalculate the ripple color and button rotation whenever the rating changes
   const rippleColor = React.useMemo(() => {
     return getRandomColor();
   }, [rating]);
+  const rotation = React.useMemo(() => {
+    return getRandomRotation();
+  }, [rating]);
 
   return (
-    <div style={{ '--ripple-color': rippleColor } as React.CSSProperties}>
+    <div
+      style={
+        {
+          '--ripple-color': rippleColor,
+          '--rotation': rotation, // Pass the random rotation as a CSS variable
+        } as React.CSSProperties
+      }
+    >
       <StarPicker //
         className="faceTheme"
         value={rating}
