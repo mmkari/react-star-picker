edd9aad408f6e6dcf6dcafd798be9d32f3c33d77
Miko Kari <miko.kari@outlook.com>
2025-03-26 21:52:59 +0200

    adjust default styles

--- docusaurus/src/components/examples/source-code/LargePicker.tsx
+++ docusaurus/src/components/examples/source-code/LargePicker.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import SourceCodeSection from '@site/src/components/SourceCodeSection';
 import MyComponentSource from '!!raw-loader!@site/src/components/HomepageFeatures/LargePicker';
-// import MyComponentStyleSource from '!!raw-loader!@site/src/css/LargePicker.css';
+import MyComponentStyleSource from '!!raw-loader!@site/src/css/LargePicker.css';
 
 const LargePicker = () => {
   return (
     <SourceCodeSection
       componentSource={MyComponentSource}
-      componentStyleSource={null}
+      componentStyleSource={MyComponentStyleSource}
     />
   );
 };
