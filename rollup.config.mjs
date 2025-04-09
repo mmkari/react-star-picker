ba902b960ef43c37ab5e4f7392ac4441ec59f27f
Miko Kari <miko.kari@outlook.com>
2025-04-09 22:40:28 +0300

    update terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,17 +2,16 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json' with { type: "json" };
-import { dts } from "rollup-plugin-dts";
-import del from "rollup-plugin-delete"
-// import preserveDirectives from "rollup-plugin-preserve-directives";
+import terser from '@rollup/plugin-terser'; // Updated to default import
+import pkg from './package.json' with { type: 'json' };
+import { dts } from 'rollup-plugin-dts';
+import del from 'rollup-plugin-delete';
 import postcss from 'rollup-plugin-postcss';
 
 const input = ['./src/index.ts'];
 
 const globals = {
-  'react': 'React',
+  react: 'React',
   'react-dom': 'ReactDOM',
 };
 
@@ -22,54 +21,34 @@ export default [
     input,
     output: [
       {
-        // file: `lib/esm/index.js`,
-        dir: "lib/esm",
+        dir: 'lib/esm',
         format: 'esm',
         exports: 'named',
         sourcemap: false,
         globals,
         banner: `'use client';`,
-        // preserveModules: true,
-        // entryFileNames: (chunkInfo) => {
-        //   if (chunkInfo.name.includes('node_modules')) {
-        //     return chunkInfo.name.replace('node_modules', 'external') + '.js';
-        //   }
-  
-        //   return '[name].js';
-        // }
       },
     ],
     plugins: [
       nodeResolve(),
-      typescript({ tsconfig: './tsconfig.esm.json' }), 
+      typescript({ tsconfig: './tsconfig.esm.json' }),
       postcss({
         minimize: { preset: 'default' },
-        extract: 'styles.css', // Extracts CSS into a separate file
+        extract: 'styles.css',
       }),
       commonjs(),
-      // preserveDirectives(),
       terser({
         compress: {
-          directives: false,
-        }
-      })
+          directives: false, // Preserve directives like 'use client'
+        },
+      }),
     ],
     external: ['react', 'react-dom'],
-
-    // Ignore warnings when using "use client" directive
-    // onwarn(warning, warn) {
-    //   if (warning.code !== 'MODULE_LEVEL_DIRECTIVE') {
-    //     warn(warning);
-    //   }
-    // },
   },
   {
-    input: "lib/esm/dts/index.d.ts",
-    output: [{ file: pkg.types, format: "esm" }],
-    plugins: [
-      dts(),
-      del({ hook: "buildEnd", targets: "./lib/esm/dts" }),
-    ],
-    external: ['react', 'react-dom', /\.css$/]
+    input: 'lib/esm/dts/index.d.ts',
+    output: [{ file: pkg.types, format: 'esm' }],
+    plugins: [dts(), del({ hook: 'buildEnd', targets: './lib/esm/dts' })],
+    external: ['react', 'react-dom', /\.css$/],
   },
 ];
